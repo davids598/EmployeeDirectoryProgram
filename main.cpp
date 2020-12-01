406d65902bc67d2b315d7c9a3ab108fc217650fa
Sean <smes@cox.net>
2020-12-01 11:20:43 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,10 @@
 
 int main(){
 	Employee* sean = new Employee("Sean", "S", "Comp sci", 50000, 11, 30, 2020, NULL) ;
-	Employee* bean = new Employee("Bean", "B", "Comp Sky", 76000, 11, 31, 2020, sean) ;
+	Employee* bean = new Employee("Bean", "B", "Comp Sci", 76000, 11, 31, 2020, sean) ;
 	employeeDirectory* e = new employeeDirectory(sean) ;
 	e->addEmployee(bean) ;
 	Printer* p = new listPrint();
-	e->set_print_strat(p) ;
-	e->print() ;
-	p = new treePrint() ;
 	e->set_print_strat(p) ;
 	e->print() ;
 
